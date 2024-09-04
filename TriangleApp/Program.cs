0bc661d72848901241686309ac232ecc2d2599a1
İsmail Emre Aydın <96790404+ismaillemreaydin@users.noreply.github.com>
2024-09-04 15:34:58 +0300

    Add files via upload

---
+++ TriangleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TriangleApp
+{
+    // Kullanıcıdan veri alma ve doğrulama işlemlerini üstlenir.
+    public static class InputHandler
+    {
+        public static int GetSizeInput()
+        {
+            Console.Write("Üçgenin boyutunu giriniz: ");
+            string input = Console.ReadLine();
+            int size;
+
+            while (!int.TryParse(input, out size) || size <= 0)
+            {
+                Console.WriteLine("Lütfen geçerli bir sayı giriniz!");
+                input = Console.ReadLine();
+            }
+
+            return size;
+        }
+    }
+
+    // Üçgen çiziminden sorumlu sınıf.
+    public class TriangleDrawer
+    {
+        private int _size;
+
+        public TriangleDrawer(int size)
+        {
+            _size = size;
+        }
+
+        public void DrawTriangle()
+        {
+            for (int i = 1; i <= _size; i++)
+            {
+                PrintLine(i);
+            }
+        }
+
+        private void PrintLine(int lineLength)
+        {
+            string line = new string('*', lineLength);
+            Console.WriteLine(line);
+        }
+    }
+
+    // Programın giriş noktası
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = InputHandler.GetSizeInput();
+            TriangleDrawer triangleDrawer = new TriangleDrawer(size);
+            triangleDrawer.DrawTriangle();
+        }
+    }
+}
